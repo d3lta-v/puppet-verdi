f45e9c906522038e4d329db457508bd6e43173be
Pan Ziyue <random.rrr3@gmail.com>
2018-11-19 11:26:39 +0800

    change ec2 public ip

--- lib/facter/ec2_public_ipv4.rb
+++ lib/facter/ec2_public_ipv4.rb
@@ -1,6 +1,6 @@
 # ec2_public_ipv4.rb
 Facter.add("ec2_public_ipv4") do
   setcode do
-    %x{curl -s http://169.254.169.254/latest/meta-data/public-ipv4}.chomp
+    %x{curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipaddress/0/publicip?api-version=2017-03-01&format=text"}.chomp
   end
 end
